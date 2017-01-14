2bc12ef786fa237c6b383817f5c2ff600b7276d4
mohamedsaad306 <makcemos_X@yahoo.com>
2017-01-14 00:11:43 -0800

    Ienum

---
+++ LinkedIn_Project/LinkedIn_Project/BLL/UserDetaiManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LinkedIn_Project.DAL;
+
+namespace LinkedIn_Project.BLL
+{
+    public static class UserDetaiManager
+    {
+        public static List<UserDetail> GetAllUsersDetails()
+        {
+            MyDBContext ctx = new MyDBContext();
+            return ctx.UserDetails.Where(u => u.IsDeleted==false).ToList();
+        }
+        public static UserDetail GetUserDetailByID(Guid id) 
+        { 
+            MyDBContext context= new MyDBContext ();
+            return (UserDetail)context.UserDetails.Where(e => e.Fk_UserId == id);
+        }
+        public static bool 
+
+    }
+}
